c0b977bb0f5b2eae2f5a7a4ca7c0eaed5933b2dd
a.teran <a.teran@PYD07ZYD6-AECIT>
2024-07-16 14:57:03 -0500

    ateran

--- ProyectoMovile/Vistas/Login.xaml.cs
+++ ProyectoMovile/Vistas/Login.xaml.cs
@@ -1,3 +1,6 @@
+using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
+
 namespace ProyectoMovile.Vistas;
 
 public partial class Login : ContentPage
@@ -7,8 +10,17 @@ public partial class Login : ContentPage
 		InitializeComponent();
 	}
 
-    private void btnAutenticar_Clicked(object sender, EventArgs e)
+    private  void btnAutenticar_Clicked(object sender, EventArgs e)
     {
-        Navigation.PushAsync(new Vistas.vMenu());
+        Navigation.PushAsync(new vMenu());
     }
+   
+    
+    private  void btnValdiarBD_Clicked(object sender, EventArgs e)
+    {
+    } 
 }
+
+
+
+
--- ProyectoMovile/Vistas/Negocio/vNegocio.xaml.cs
+++ ProyectoMovile/Vistas/Negocio/vNegocio.xaml.cs
@@ -1,9 +1,25 @@
+using Newtonsoft.Json;
+using System.Collections.ObjectModel;
+
 namespace ProyectoMovile.Vistas.Negocio;
 
 public partial class vNegocio : ContentPage
 {
-	public vNegocio()
+
+    private const string url = "http://localhost:81/Proyecto/selectCliente.php";
+    private readonly HttpClient cliente = new HttpClient();
+    private ObservableCollection<Modelos.Cliente> clientes;
+    public vNegocio()
 	{
 		InitializeComponent();
+        obtener();
 	}
+    public async void obtener()
+    {
+        var content = await cliente.GetStringAsync(url);
+        List<Modelos.Cliente> mostrarCli = JsonConvert.DeserializeObject<List<Modelos.Cliente>>(content);
+        clientes = new ObservableCollection<Modelos.Cliente>(mostrarCli);
+        listaClientes.ItemsSource = clientes;
+
+    }
 }
